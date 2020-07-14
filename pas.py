5f9655129ccb8e475d1fbac121e1d000eb9b7ccc
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-14 13:16:54 +0300

    ft:starting_out_pandas_tutorial:backend

---
+++ pas.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+
+print(pd)
+df = pd.read_csv('pokemon_data.csv')
+# print(df.head(4))
+print(df['Defense'])
+##Read headers
+print(df.columns)
+##Reacd each column 
+# print(df[['Name','Type 1','HP']])
+##df slicing
+print(df.iloc[0:4])
+##Read each row 
+for index,row in df.iterrows():
+    print(index,row['Name'])
+
+##Search for a specific row name
+df.loc(df['Type 1' == "Grass"])
