c4b5cce5e77cd35b42f8d0644645882e578a9eca
manulangat <emmanuelthedeveloper@gmail.com>
2020-07-14 13:20:12 +0300

    ft:reading_data:pandas

--- pas.py
+++ pas.py
@@ -11,8 +11,12 @@ print(df.columns)
 ##df slicing
 print(df.iloc[0:4])
 ##Read each row 
-for index,row in df.iterrows():
-    print(index,row['Name'])
+# for index,row in df.iterrows():
+#     print(index,row['Name'])
 
 ##Search for a specific row name
-df.loc(df['Type 1' == "Grass"])
+# df.loc(df['Type 1' == "Grass"])
+print(df.loc[df['Type 1'] == "Grass"])
+
+##Read a specific location
+print(df.iloc[9,1])
